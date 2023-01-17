c8468c19bbf38b1b34e03d42f39c38e7c61cc963
Home <Home@LAPTOP-2E6V6ACM>
2023-01-17 12:11:39 +0200

    Example

--- StartWithMaui/MainPage.xaml.cs
+++ StartWithMaui/MainPage.xaml.cs
@@ -13,5 +13,21 @@ public partial class MainPage : ContentPage
 	{
 		
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+        int i;
+		string no = img.Source.ToString()[7].ToString();
+		i = int.Parse(no.ToString());
+        if (button==up)
+		{ i = i + 1; }
+		else
+		{
+			i = i - 1;
+		}
+		
+		img.Source = $"a{i%5}a.jpg";
+	}
 }
 
