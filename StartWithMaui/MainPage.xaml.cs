1e2ef8164bec10b3ccee5d2d862ecaf4ea5c5ef6
Home <Home@LAPTOP-2E6V6ACM>
2023-01-17 13:09:56 +0200

    1

--- StartWithMaui/MainPage.xaml.cs
+++ StartWithMaui/MainPage.xaml.cs
@@ -2,11 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	
-
+	List<string> imageUrls;
+	int pos;
 	public MainPage()
 	{
 		InitializeComponent();
+		imageUrls = new List<string>() { "https://media.tenor.com/Q2RZ9YrQ14oAAAAM/omg-wow.gif", "https://media4.giphy.com/media/5VKbvrjxpVJCM/giphy.gif", "https://i.pinimg.com/originals/e2/c0/8a/e2c08adfc309967679935d0dccb89cf9.gif" };
+		pos = -1;
+		down.IsEnabled = false;
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
@@ -17,17 +20,54 @@ public partial class MainPage : ContentPage
 	private void Button_Clicked(object sender, EventArgs e)
 	{
 		Button button = (Button)sender;
-        int i;
-		string no = img.Source.ToString()[7].ToString();
-		i = int.Parse(no.ToString());
-        if (button==up)
-		{ i = i + 1; }
+
+
+		//      int i;
+		//string no = img.Source.ToString()[7].ToString();
+		//i = int.Parse(no.ToString());
+
+
+
+		if (button == up)
+		{
+			pos = pos + 1;
+			if (pos >= imageUrls.Count - 1)
+			{
+				up.IsEnabled = false;
+
+			}
+
+		}
 		else
 		{
-			i = i - 1;
+			pos = pos - 1;
+			if (pos <= 0)
+			{
+				down.IsEnabled = false;
+
+			}
 		}
+
+			
 		
-		img.Source = $"a{i%5}a.jpg";
-	}
+		img.Source = imageUrls[pos];
+
+        if (pos <= 0)
+        {
+            down.IsEnabled = false;
+            up.IsEnabled = true;
+        }
+       else if (pos >= imageUrls.Count-1)
+        {
+            up.IsEnabled = false;
+            down.IsEnabled = true;
+
+        }
+		else
+		{
+            down.IsEnabled = true;
+            up.IsEnabled = true;
+        }
+    }
 }
 
